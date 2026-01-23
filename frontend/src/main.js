e73ef68f3ebfe0bc28210b3b6ac697c1c373128f
LJ <3079196594@qq.com>
2026-01-23 11:03:16 +0800

    第2次提交

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css' // 引入我们配置好的 Tailwind 样式
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
